7fe490d01b628a85c144f3552d2f05b9f3f56173
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-16 12:30:57 +0100

    refactorice

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,24 @@
 'use strict';
 
-function myFunction(event) {
+const username = document.querySelector("#username");
+const name = document.querySelector("#full-name");
+const bio = document.querySelector("#bio");
+const image = document.querySelector("#image");
+const notFound = document.querySelector("#not-found");
+const ok = document.querySelector("#ok");
+const search = document.querySelector("#search");
+
+search.addEventListener('click', handleClick)
+
+function handleClick(event) {
   const value = document.getElementById("valueInput").value;
   event.preventDefault();
-  fetchData(value)
+  resetNotFound();
+  fetchData(value);
+}
+
+function resetNotFound() {
+  notFound.style.display = "none";
 }
 
 function fetchData(value) {
@@ -11,26 +26,18 @@ function fetchData(value) {
     .then(response => {
       return response.json();
     })
-    .then(data => {
-      ok.style.display = "block";
-
-      const username = document.querySelector("#username");
-      const name = document.querySelector("#full-name");
-      const bio = document.querySelector("#bio");
-      const image = document.querySelector("#image");
-  
+    .then(data => {  
       name.innerHTML = data.name;
       username.innerHTML = data.login;
       bio.innerHTML = data.bio;
       image.src = data.avatar_url;
-  
+      ok.style.display = "block";
+
       if (data.message === "Not Found") {
-        const notFound = document.querySelector("#not-found");
-        const ok = document.querySelector("#ok");
-  
         notFound.style.display = "block";
         ok.style.display = "none";
       }
+
     })
     .catch(err => {
       console.log("error ->", err);
@@ -41,32 +48,24 @@ function fetchData(value) {
       return response.json();
     })
     .then(data => {
-      for (let i = 0; i < 5; i++) {
-        const reponame = document.querySelector("#repo-name");
-        const stars = document.querySelector("#stars");
-        const fork = document.querySelector("#fork");
-        const repositories = document.querySelector("#repositories");
-  
-        const reponameData = (reponame.innerHTML = data[i].name);
-        const starsData = (stars.innerHTML = data[i].stargazers_count);
-        const forkData = (fork.innerHTML = data[i].forks);
-  
-        const repo =
-          '<div class="repo"> <p id="repo-name">' +
-          reponameData +
-          '</p><div class="starts-forks"><img src="./img/star-solid.svg" alt="star"><p id="stars">' +
-          starsData +
-          '</p><img src="./img/code-branch-solid.svg" alt="fork"><p id="fork">' +
-          forkData +
-          "</p></div></div>";
-  
-        repositories.innerHTML += repo;
-      }
+      const repos = data.map( repo => {
+        const reponame = repo.name;
+        const stars = repo.stargazers_count;
+        const fork = repo.forks;
+        return `<div class="repo">
+            <p id="repo-name">${reponame}</p>
+            <div class="starts-forks">
+              <img src="./img/star-solid.svg" alt="star"><p>${stars}</p>
+              <img src="./img/code-branch-solid.svg" alt="fork"><p>${fork}</p>
+            </div>
+        </div>`;
+      });
+      const repositories = document.querySelector("#repositories");
+      repositories.insertAdjacentHTML('beforeend', repos.join(''));
     })
     .catch(err => {
       console.log("error ->", err);
-    });
-  
+    }); 
 }
 
 
